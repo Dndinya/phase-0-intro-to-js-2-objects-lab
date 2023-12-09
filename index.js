7f0b0537aa5d2a4c7ef7e13ee8ff599df628d38f
Derrick <derrick.ndinya@student.moringaschool.com>
2023-12-09 07:49:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {};
+employee.name = "Derrick";
+employee.streetAddress = "Nairobi";
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+      };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name){
+    let newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+    return typeof newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    //let newEmployee = {...employee};
+    delete employee.name ;
+    return employee;
+}
+
